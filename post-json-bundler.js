97c536480e2d3a1c6cc0ba74931a8e56c37f735f
Marcos Passos <marcos@croct.com>
2025-03-21 18:48:49 -0300

    Normalize schema IDs

---
+++ post-json-bundler.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const file = process.argv[2];
+
+const content = fs.readFileSync(file, 'utf-8')
+    .replace(
+        /"#\/definitions\/https%3A~1~1schema\.croct\.com~1json~1(.+?)\.json"/g,
+        (_, match) => `"#/definitions/${match.replace(/~1/g, '-')}"`,
+    )
+    .replace(
+        /"https:\/\/schema.croct.com\/json\/(.+?)\.json"/g,
+        (_, match) => `"${match.replace(/\//g, '-')}"`
+    );
+
+
+fs.writeFileSync(file, content, 'utf-8');
