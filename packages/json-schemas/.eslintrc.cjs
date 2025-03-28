d9ac98b410331ca6d55a94b6e78a6e739516786a
Marcos Passos <marcospassos@users.noreply.github.com>
2025-03-28 10:34:26 -0300

    Add tests (#2)

---
+++ packages/json-schemas/.eslintrc.cjs
@@ -0,0 +1,18 @@
+// Workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    extends: ['plugin:@croct/typescript'],
+    plugins: ['@croct'],
+    parserOptions: {
+        project: ['**/tsconfig.json'],
+    },
+    overrides: [
+        {
+            files: ['jest.config.js'],
+            rules: {
+                'import/no-default-export': 'off',
+            },
+        },
+    ],
+};
