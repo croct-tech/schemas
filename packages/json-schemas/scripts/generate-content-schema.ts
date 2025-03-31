025708ad56f94877e1d05268b429272afebb9592
Marcos Passos <marcospassos@users.noreply.github.com>
2025-03-31 17:14:34 -0300

    Add schemas for content model (#3)

---
+++ packages/json-schemas/scripts/generate-content-schema.ts
@@ -0,0 +1,24 @@
+import lenientSchema from '@croct/content-model/content/schema.json';
+import {writeFileSync} from 'fs';
+import {JSONSchema} from 'json-schema-typed';
+
+const schemaPath = process.argv[2];
+
+if (schemaPath === undefined) {
+    console.error('Usage: generate-content-schema <schema-path>');
+
+    process.exit(1);
+}
+
+const schema: JSONSchema.Object = {
+    $schema: 'https://json-schema.org/draft/2019-09/schema',
+    ...(lenientSchema as unknown as JSONSchema.Object),
+    properties: {
+        $schema: {
+            type: 'string',
+        },
+        ...lenientSchema.properties,
+    },
+};
+
+writeFileSync(schemaPath, JSON.stringify(schema, null, 2), 'utf-8');
